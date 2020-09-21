cb737c86af6f32b1739067d4904e42e5d33ec1a5
GabrielIFPB <jeronimogbariel@gmail.com>
2020-09-21 18:41:27 -0300

    iniciando projeto

---
+++ app/src/main/java/com/inteligenciadigital/androidgpslocationtracker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.inteligenciadigital.androidgpslocationtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
